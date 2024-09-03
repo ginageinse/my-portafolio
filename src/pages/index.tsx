e2a7aaf70eeecc0989e7b5875194597d5e78b59a
ginageinse <ginageinse@gmail.com>
2024-09-02 21:59:51 -0400

    fix: build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,13 @@ import WelcomeText from '../components/WelcomeText';
 
 const HomePage: React.FC = () => (
   <Layout>
-    {/* Componente de texto animado */}
     <WelcomeText />
-    
-    {/* Contenido original de la página */}
     <div className="mt-10">
       <h2 className="text-2xl font-semibold mb-4 text-dark-gray">Bienvenido a mi portafolio</h2>
       <p className="text-lg mb-6 text-dark-gray">Aquí puedes encontrar mis proyectos y más información sobre mí.</p>
-      <Button onClick={() => window.open('https://wa.me/56989030326?text=Hola,%20me%20interesa%20tu%20perfil', '_blank')}>¡Contáctame!</Button>
+      <Button onClick={() => window.open('https://wa.me/56989030326?text=Hola,%20me%20interesa%20tu%20perfil', '_blank')}>
+        ¡Contáctame!
+      </Button>
     </div>
   </Layout>
 );
